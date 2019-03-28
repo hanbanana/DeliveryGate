853dc707a3b3f7ed82e55855efe93624f342d0aa
Hansung Lee <hanlee@GOLD-POINT.COM>
2019-03-28 08:18:32 -0700

    added edit page and function

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var connection = mysql.createConnection({
   database: "hrx_delivery"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
@@ -35,8 +35,8 @@ connection.connect(function(err) {
 });
 
 // Use Handlebars to render the main index.html page with the todos in it.
-app.get("/", function(req, res) {
-  connection.query("SELECT * FROM deliveryOrder;", function(err, data) {
+app.get("/", function (req, res) {
+  connection.query("SELECT * FROM deliveryOrder;", function (err, data) {
     if (err) {
       return res.status(500).end();
     }
@@ -45,8 +45,8 @@ app.get("/", function(req, res) {
   });
 });
 
-app.get("/create", function(req, res) {
-  connection.query("SELECT * FROM deliveryOrder;", function(err, data) {
+app.get("/create", function (req, res) {
+  connection.query("SELECT * FROM deliveryOrder;", function (err, data) {
     if (err) {
       return res.status(500).end();
     }
@@ -55,23 +55,44 @@ app.get("/create", function(req, res) {
   });
 });
 
-// Create a new todo
-app.post("/todos", function(req, res) {
-  connection.query("INSERT INTO deliveryOrder (BL, Terminal_Name, Container_No, Vassel_No, ETA, Weight, Seal_No, Delivery_Location, Status_, Return_, Close_) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
-  [req.body.BL, req.body.Terminal_Name, req.body.Container_No, req.body.Vassel_No, req.body.ETA, req.body.Weight, req.body.Seal_No, req.body.Delivery_Location, req.body.Status_, req.body.Return_, req.body.Close_], function(err, result) {
+app.get("/:id", function (req, res) {
+  connection.query("SELECT * FROM deliveryOrder where id = ?", [req.params.id], function (err, data) {
     if (err) {
       return res.status(500).end();
     }
 
-    // Send back the ID of the new todo
-    res.json({ id: result.insertId });
-    console.log({ id: result.insertId });
+    console.log(data);
+    res.render("edit", data[0]);
   });
 });
 
+app.get("/customer", function (req, res) {
+  connection.query("SELECT * FROM deliveryOrder;", function (err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.render("customer", { deliveryOrder: data });
+  });
+});
+
+// Create a new todo
+app.post("/todos", function (req, res) {
+  connection.query("INSERT INTO deliveryOrder (BL, Terminal_Name, Container_No, Vassel_No, ETA, Weight, Seal_No, Delivery_Location, Status_, Return_, Close_) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    [req.body.BL, req.body.Terminal_Name, req.body.Container_No, req.body.Vassel_No, req.body.ETA, req.body.Weight, req.body.Seal_No, req.body.Delivery_Location, req.body.Status_, req.body.Return_, req.body.Close_], function (err, result) {
+      if (err) {
+        return res.status(500).end();
+      }
+
+      // Send back the ID of the new todo
+      res.json({ id: result.insertId });
+      console.log({ id: result.insertId });
+    });
+});
+
 // Retrieve all todos
-app.get("/todos", function(req, res) {
-  connection.query("SELECT * FROM deliveryOrder;", function(err, data) {
+app.get("/todos", function (req, res) {
+  connection.query("SELECT * FROM deliveryOrder;", function (err, data) {
     if (err) {
       return res.status(500).end();
     }
@@ -81,24 +102,26 @@ app.get("/todos", function(req, res) {
 });
 
 // Update a todo
-app.put("/todos/:id", function(req, res) {
-  connection.query("UPDATE deliveryOrder SET BL = ? WHERE id = ?", [req.body.BL, req.params.id], function(err, result) {
-    if (err) {
-      // If an error occurred, send a generic server failure
-      return res.status(500).end();
-    }
-    else if (result.changedRows === 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    }
-    res.status(200).end();
+app.put("/todos/:id", function (req, res) {
+  connection.query("UPDATE deliveryOrder SET BL = ?, Terminal_Name = ?, Container_No = ?, Vassel_No = ?, ETA = ?, Weight = ?, Seal_No = ?, Delivery_Location = ?, Status_ = ?, Return_ = ?, Close_ = ? WHERE id = ?",
+  [req.body.BL, req.body.Terminal_Name, req.body.Container_No, req.body.Vassel_No, req.body.ETA, req.body.Weight, req.body.Seal_No, req.body.Delivery_Location, req.body.Status_, req.body.Return_, req.body.Close_, req.params.id],
+    function (err, result) {
+      if (err) {
+        // If an error occurred, send a generic server failure
+        return res.status(500).end();
+      }
+      else if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
 
-  });
+    });
 });
 
 // Delete a todo
-app.delete("/todos/:id", function(req, res) {
-  connection.query("DELETE FROM deliveryOrder WHERE id = ?", [req.params.id], function(err, result) {
+app.delete("/todos/:id", function (req, res) {
+  connection.query("DELETE FROM deliveryOrder WHERE id = ?", [req.params.id], function (err, result) {
     if (err) {
       // If an error occurred, send a generic server failure
       return res.status(500).end();
@@ -113,7 +136,7 @@ app.delete("/todos/:id", function(req, res) {
 });
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
